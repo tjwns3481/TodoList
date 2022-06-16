1da2edff16dd78ef190fe395bd07ab33ccb09ebc
tjwns3481 <tjwns3481@gmail.com>
2022-06-16 17:15:45 +0900

    220616

--- js/common.js
+++ js/common.js
@@ -5,15 +5,18 @@
 3. 선택 목록 삭제하기
 4. 전체 목록 삭제하기 
 
-<수정사항>
+<1차 수정사항>
 1. css / html 폴더별로 파일 나눠서 정리
 2. html 태그에 직접 이벤트 삽입 하지 말고 script에서 호출해주기
 3. 체크한 리스트만 삭제하기 기능 추가
 
+<2차 수정사항>
+1. 항목 완료 처리 기능 추가
+2. 수정하기 기능 추가
+3. 체크리스트지우기 jquery -> js로 바꾸기
 */
 window.onload = function () { 
-  let listIndex = 0;
-  const listBox = document.querySelector('ul');
+  const listBox = document.querySelector('#todo_ul');
   const makeBtn = document.querySelector('#inputValue');
   const deleteAllTarget = document.querySelector('#del_all_box');
   const checkedClearBtn = document.querySelector('.del_checked_box');
@@ -28,30 +31,27 @@ window.onload = function () {
 
   //리스트 만들기
   function makeTodo() {
-    const name = document.getElementById('inputValue').value;
-    const listLi = document.createElement('li');
+    const listLi = document.createElement('li')
+    const name = document.querySelector('#inputValue').value;
     const delBtn = document.createElement('span')
+    const checkedInput = document.querySelector('input')
+    const isChecked = checkedInput.getAttribute('checked')
+    const labelId = Date.now();
 
-    delBtn.id = 'delete'+listIndex
-    listLi.innerHTML = `<input type="checkbox"><label>${name}</label>`;
+    console.log(isChecked)
+    listLi.innerHTML = `<input type="checkbox"><label id="${labelId}">${name}</label>`;
     delBtn.innerHTML = `삭제`
-    document.getElementById('inputValue').value = null
+    document.querySelector('#inputValue').value = null
     !name || name[0] === ' ' ? null : listBox.appendChild(listLi) && listLi.appendChild(delBtn);
 
-    delBtn.addEventListener('click',function() {
+    delBtn.addEventListener('click', function () {
       delTodo(listLi)
     })
-
-    listIndex++;
-    //리스트 지우기
-    // $('li.todoLi > span.delete').on('click', function () {
-    //   $(this).parent().remove();
-    // })
   }
 
   //리스트 지우기
   function delTodo(element) {
-    document.getElementById('todo_ul').removeChild(element)
+    document.querySelector('#todo_ul').removeChild(element)
   }
 
   //모든 리스트 지우기
@@ -63,16 +63,20 @@ window.onload = function () {
     }
   }
 
+  //항목 완료 처리 
+
+
   //체크 리스트 지우기
   function clearChecked() {
-    const listUl = $('#todo_ul');
-    const listLi = $(listUl).children()
-    const checkedLi = $(listLi).children('input').is(':checked')
+    // const listUl = $('#todo_ul');
+    // const listLi = $(listUl).children()
+    // const checkedLi = $(listLi).children('input').is(':checked')
 
-    $(listLi).each(function () {
-      if (checkedLi) {
-        $($(listLi).children('input:checked')).parent().remove()
-      }
-    })
+
+    // $(listLi).each(function () {
+    //   if (checkedLi) {
+    //     $($(listLi).children('input:checked')).parent().remove()
+    //   }
+    // })
   }
 }
